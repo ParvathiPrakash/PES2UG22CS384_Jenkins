4f9ee20a2b330e6c243b6f8a0d52136196732f81
Parvathi <paarutm25@gmail.com>
2025-03-14 15:25:01 +0530

    updated cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, I'm Parvathi\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this. Bye \n";
   return 0;
 }
